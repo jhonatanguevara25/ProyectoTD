473d3bbef8f7efd8530206f190551597c6304bf6
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-09-24 17:38:34 -0500

    Include categoria

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const routes = require('./routes')
+const routes = require("./routes");
+const routesCategoria = require("./routesCategoria");
 
-const app = express()
-app.set('port', process.env.PORT || 9000)
-
+const app = express();
+app.set("port", process.env.PORT || 9000);
 
 // middlewares -------------------------------------
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 // routes -------------------------------------------
-app.get('/', (req, res)=>{
-    res.send("Api funcionando")
-})
+app.get("/", (req, res) => {
+  res.send("Api funcionando");
+});
 
-app.use('/api/bodeguero', routes)
+app.use("/api/bodeguero", routes);
+app.use("/api/categoria", routesCategoria);
 
 // server running -----------------------------------
-app.listen(app.get('port'), ()=>{
-    console.log('server running on port', app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log("server running on port", app.get("port"));
+});
