e0f0e140b6fd1525b6cc8d16e40ae3eacbc70eda
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-09-29 23:06:28 -0500

    Update - Styles

--- script/crud.js
+++ script/crud.js
@@ -2,14 +2,16 @@
 //Inicializando la tabla
 const tablaBodeguero = document.getElementById("tabla-bodeguero");
 tablaBodeguero.innerHTML="";
-tablaBodeguero.innerHTML+="<table><tr class='header'><td style='width: 100px;'>Id</td><td>Nombre Completo</td><td>Correo</td><td>Password</td><td style='width: 150px;'>Suscripcion</td><td class='tdButtom'>  </td><td class='tdButtom'>  </td></tr>"
+tablaBodeguero.innerHTML+="<table><tr class='header'><td style='width: 100px;'>Id</td><td>Nombre Completo</td><td>Correo</td><td>Password</td><td style='width: 150px;'>Suscripcion</td><td style='width: 60px;'>  </td><td style='width: 60px;'>  </td></tr>"
 
 
 fetch(`http://localhost:9000/api/bodeguero`).then((response) => {
   response.json().then((data) => {
         const bodegueros = data[0];
         for(let item = 0; item < bodegueros.length;item++){
-            console.log( bodegueros[item]);
+            let nombreURL = bodegueros[item].nombreCompleto.replaceAll(" ", "%");
+            console.log(nombreURL);
+
             //tablaBodeguero.innerHTML+=`<tr><td>"+  ${bodegueros[item].idAdmin + "}</td><td>  ${bodegueros[item].nombreCompleto+"}</td><td>  ${bodegueros[item].password+"}</td><td>  ${bodegueros[item].suscripcion+"</td></tr><br>`;
             tablaBodeguero.innerHTML+=`<table>
             <tr><td style='width: 100px;'>${bodegueros[item].idAdmin}</td>
@@ -17,8 +19,8 @@ fetch(`http://localhost:9000/api/bodeguero`).then((response) => {
             <td>${bodegueros[item].correo}</td>
             <td>  ${bodegueros[item].password} </td>
             <td style='width: 150px;'>  ${bodegueros[item].suscripcion} </td>
-            <td class="tdButtom"> <button>U</button> </td>
-            </td><td class="tdButtom"> <button onclick="deleteBod(${bodegueros[item].idAdmin})">D</button> </td>
+            <td style='width: 60px;'><button class="tdButtomUp"> <a href=/update?id=${bodegueros[item].idAdmin}&nombre=${nombreURL}&correo=${bodegueros[item].correo}>U</a> </button></td>
+            </td><td style='width: 60px;'> <button class="tdButtom" onclick="deleteBod(${bodegueros[item].idAdmin})">D</button> </td>
             </tr></table>`;
         }
     });
